6c2b68d05a3ea5f106cb5fc28d52746b95ecc536
Elchanan5760 <emstone@gmail.com>
2025-05-27 11:59:16 +0300

    real weapon

---
+++ weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commandos.models
+{
+    public class Weapon
+    {
+        private string _name;
+        private string _manufacturer;
+        private int _bulletSapply;
+        public Weapon(string name,string manufacturer,int bulletSapply)
+        {
+            _name = name;
+            _manufacturer = manufacturer;
+            _bulletSapply = bulletSapply;
+        }
+
+        public void Shoot()
+        {
+            _bulletSapply -= 1;
+            Console.WriteLine("Shooting...\n" +
+                $"{_bulletSapply} bullet left"); 
+        }
+    }
+}
