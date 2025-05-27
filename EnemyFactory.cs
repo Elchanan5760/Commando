4cbdb8f2beb6947e3c6d3901dd70f3df673d7ace
Elchanan5760 <emstone@gmail.com>
2025-05-27 16:22:25 +0300

    enemy factory

---
+++ EnemyFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commandos
+{
+    public class EnemyFactory
+    {
+        private string _name;
+        private int _life;
+        private string _lifeStatus;
+        public EnemyFactory(string name , int life , string lifeStatus)
+        {
+            _name = name;
+            _life = life;
+            _lifeStatus = lifeStatus;
+        }
+        public void Yelling()
+        {
+            Console.WriteLine("I am an enemy");
+        }
+    }
+}
